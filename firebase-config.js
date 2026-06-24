e53d90c67ac96b91b773ac5008fc62fdc8a5a00b
Leonardo <souzaleonardo2003@hotmail.com>
2026-06-23 22:56:48 -0300

    Corrige configuração do Firebase

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyABAJWG2O2VeIr_tI-P2R3c9fos1WW0nvY",
+  authDomain: "granja-app-6f53c.firebaseapp.com",
+  projectId: "granja-app-6f53c",
+  storageBucket: "granja-app-6f53c.firebasestorage.app",
+  messagingSenderId: "938868134235",
+  appId: "1:938868134235:web:9e2cf2cbb9f0ba87c1ca7e"
+};
